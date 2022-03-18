e1bb41885e5065819e9cc6009f7c5fdbbbfe417b
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2022-03-18 02:11:57 -0300

    🐛 BUG: Heroku

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { ValidationPipe } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-
-import './database'
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import './database';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
